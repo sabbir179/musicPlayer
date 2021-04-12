9c059a773ae486be82086b44c90ca5ed7da4ffa4
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2021-04-12 14:13:25 +0600

    modification on css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn = document.querySelector('#play')
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next)
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
